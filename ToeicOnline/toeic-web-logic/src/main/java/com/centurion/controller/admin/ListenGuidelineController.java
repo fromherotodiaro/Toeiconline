a0919c7a6e6603969f5fe792837072720d2a3e7b
fromherotodiaro <fromherotodiaro@gmail.com>
2022-07-24 15:34:24 +0700

    urlPartterns and Upload file using Apache Commons

--- ToeicOnline/toeic-web-logic/src/main/java/com/centurion/controller/admin/ListenGuidelineController.java
+++ ToeicOnline/toeic-web-logic/src/main/java/com/centurion/controller/admin/ListenGuidelineController.java
@@ -9,8 +9,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.fileupload.FileUploadException;
 
 import com.centurion.command.ListenGuideLineCommand;
+import com.centurion.core.commons.utils.UploadUtil;
 import com.centurion.core.web.common.WebConstant;
 import com.centurion.core.web.utils.FormUtil;
 
@@ -25,6 +29,7 @@ public class ListenGuidelineController extends HttpServlet {
 
 		ListenGuideLineCommand command = FormUtil.populate(ListenGuideLineCommand.class, req);
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("ApplicationResources");
+		HttpSession session = req.getSession();
 
 //		command.setMaxPageItems(2);
 //		RequestUtil.initSearchBear(req, command);
@@ -34,6 +39,10 @@ public class ListenGuidelineController extends HttpServlet {
 //		command.setTotalItems(Integer.parseInt(objects[0].toString()));
 //		req.setAttribute(WebConstant.ALERT, WebConstant.TYPE_SUCCESS);
 //		req.setAttribute(WebConstant.MESSAGE_RESPONSE, resourceBundle.getString("label.guideline.listen.add.success"));
+		if (session != null) {
+			req.setAttribute(WebConstant.ALERT, session.getAttribute(WebConstant.ALERT));
+			req.setAttribute(WebConstant.MESSAGE_RESPONSE, session.getAttribute(WebConstant.MESSAGE_RESPONSE));
+		}
 
 		req.setAttribute(WebConstant.LIST_ITEMS, command);
 		if (command.getUrlType() != null && command.getUrlType().equals(WebConstant.URL_LIST)) {
@@ -43,13 +52,33 @@ public class ListenGuidelineController extends HttpServlet {
 			RequestDispatcher rd = req.getRequestDispatcher("/views/admin/listenguideline/edit.jsp");
 			rd.forward(req, resp);
 		}
+		session.removeAttribute(WebConstant.ALERT);
+		session.removeAttribute(WebConstant.MESSAGE_RESPONSE);
 
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		ResourceBundle bundle = ResourceBundle.getBundle("ApplicationResources");
+		UploadUtil uploadUtil = new UploadUtil();
+		HttpSession session = req.getSession();
+		try {
+			uploadUtil.writeOrUpdateFile(req);
+			/*
+			 * request.setAttribute(WebConstant.ALERT, WebConstant.TYPE_SUCCESS);
+			 * request.setAttribute(WebConstant.MESSAGE_RESPONSE,
+			 * bundle.getString("label.guideline.listen.add.success"));
+			 */
+			session.setAttribute(WebConstant.ALERT, WebConstant.TYPE_SUCCESS);
+			session.setAttribute(WebConstant.MESSAGE_RESPONSE, bundle.getString("label.guideline.listen.add.success"));
+		} catch (FileUploadException e) {
+			req.setAttribute(WebConstant.ALERT, WebConstant.TYPE_ERROR);
+			req.setAttribute(WebConstant.MESSAGE_RESPONSE, bundle.getString("label.error"));
+		} catch (Exception e) {
+			req.setAttribute(WebConstant.ALERT, WebConstant.TYPE_ERROR);
+			req.setAttribute(WebConstant.MESSAGE_RESPONSE, bundle.getString("label.error"));
+		}
+		resp.sendRedirect("admin-guideline-listen-edit.html?urlType=url_list");
 	}
 
 }
