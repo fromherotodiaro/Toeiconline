8a987e197d202b0f514c9bef35179ad9ce387c46
fromherotodiaro <fromherotodiaro@gmail.com>
2022-08-26 21:32:10 +0700

    improve search listenguideline

--- ToeicOnline/toeic-core-data/src/main/java/com/centurion/core/data/daoimpl/AbstractDao.java
+++ ToeicOnline/toeic-core-data/src/main/java/com/centurion/core/data/daoimpl/AbstractDao.java
@@ -43,12 +43,6 @@ public class AbstractDao<ID extends Serializable, T> implements GerericDao<ID, T
 		this.persistenceClass = persistenceClass;
 	}
 
-	/*
-	 * protected Session getSession() {
-	 * 
-	 * return HibernateUtil.getSessionFactory().openSession(); }
-	 */
-
 	@Override
 	public List<T> findAll() {
 
@@ -145,15 +139,12 @@ public class AbstractDao<ID extends Serializable, T> implements GerericDao<ID, T
 		return result;
 	}
 
-//findByProperty trả về số bản ghi và danh sách
-
 	List<T> list = new ArrayList<T>();
 
 	@Override
 	public Object[] findByProperty(Map<String, Object> property, String sortExpression, String sortDirection,
 			Integer offset, Integer limit) {
-		// sortExpression columns
-		// sortDirection 1 or 2
+
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		Transaction transaction = session.beginTransaction();
 		Object totalItem = 0;
@@ -168,76 +159,41 @@ public class AbstractDao<ID extends Serializable, T> implements GerericDao<ID, T
 		}
 
 		try {
-
 			StringBuilder sql1 = new StringBuilder("from ");
-			sql1.append(getPersistenceClassName());
-
+			sql1.append(getPersistenceClassName()).append(" where 1 = 1 ");
 			if (property.size() > 0) {
-
 				for (int i1 = 0; i1 < params.length; i1++) {
-					if (i1 == 0) {
-						sql1.append(" where ").append(params[i1]).append("= :" + params[i1] + "");
-					} else {
-						sql1.append(" and ").append(params[i1]).append("= :" + params[i1] + "");
-					}
-
+					sql1.append(" and ").append("LOWER(" + params[i1] + ") LIKE '%' || :" + params[i1] + " || '%'");
 				}
 
 			}
-
-//			if (property != null && value != null) {
-//				sql1.append(" where ").append(property).append("= :value");
-//			}
 			if (sortExpression != null && sortDirection != null) {
 				sql1.append(" order by ").append(sortExpression);
 				sql1.append(" " + (sortDirection.equals(CoreConstant.SORT_ASC) ? "asc" : "desc"));
 			}
 			Query query1 = session.createQuery(sql1.toString());
-
-//			if (value != null) {
-//				query1.setParameter("value", value);
-//			}
-
 			if (property.size() > 0) {
-
 				for (int i2 = 0; i2 < params.length; i2++) {
 					query1.setParameter(params[i2], values[i2]);
 				}
 			}
-
 			if (offset != null && offset >= 0) {
 
 				query1.setFirstResult(offset);
 			}
-
 			if (limit != null && limit > 0) {
 				query1.setMaxResults(limit);
 			}
-
 			list = query1.list();
-
 			StringBuilder sql2 = new StringBuilder("select count(*) from ");
-			sql2.append(getPersistenceClassName());
-
-//			if (property != null && value != null) {
-//				sql2.append(" where ").append(property).append("= :value");
-//			}
+			sql2.append(getPersistenceClassName()).append(" where 1 = 1 ");
 			if (property.size() > 0) {
-
 				for (int k1 = 0; k1 < params.length; k1++) {
-					if (k1 == 0) {
-						sql2.append(" where ").append(params[k1]).append("= :" + params[k1] + "");
-					} else {
-						sql2.append(" and ").append(params[k1]).append("= :" + params[k1] + "");
-					}
-
+					sql2.append(" and ").append("LOWER(" + params[k1] + ") LIKE '%' || :" + params[k1] + " || '%'");
 				}
 			}
 
 			Query query2 = session.createQuery(sql2.toString());
-//			if (value != null) {
-//				query2.setParameter("value", value);
-//			}
 			if (property.size() > 0) {
 
 				for (int k2 = 0; k2 < params.length; k2++) {
